d396a72fc4aa90c0ef0d2ac58219f12960a513b6
ervin-sungkono <ervinsung@gmail.com>
2024-01-31 16:27:10 +0700

    update config, add pages

--- middleware.js
+++ middleware.js
@@ -1,3 +1,5 @@
 export { default } from "next-auth/middleware"
 
-export const config = { matcher: ["/((?!register|login).*)"] }
+export const config = { matcher: [/*"/((?!register|login).*)"*/] }
+
+// NOTE: jangan uncomment matcher kecuali fitur login register sudah jadi sepenuhnya
