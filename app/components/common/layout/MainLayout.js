abcc304039684634762fab789b00818cb441d7ec
ervin-sungkono <ervinsung@gmail.com>
2024-02-04 14:04:49 +0700

    add MainLayout

---
+++ app/components/common/layout/MainLayout.js
@@ -0,0 +1,12 @@
+import Navbar from "../navbar/Navbar"
+
+export default function MainLayout({ children, hideSidebar }){
+    return(
+        <div className="flex fixed top-0 left-0 bottom-0 right-0 bg-gray-100 pt-20">
+            <Navbar hideMenu/>
+            <main className="container max-h-full mx-auto overflow-auto">
+                {children}
+            </main>
+        </div>
+    )
+}
