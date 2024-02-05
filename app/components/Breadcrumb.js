3d5ada9eee623c2ec1d8a6dc9776e2d23eaf6ff1
ervin-sungkono <ervinsung@gmail.com>
2024-02-05 10:47:35 +0700

    add header and breadcrumb component

---
+++ app/components/Breadcrumb.js
@@ -0,0 +1,24 @@
+import Link from "next/link"
+import { IoIosArrowForward as ArrowIcon } from "react-icons/io"
+
+export default function Breadcrumb({ links }){
+    return (
+      <p className="text-dark-blue/80 text-sm md:text-base flex items-center gap-1.5">
+        {links.map(({url, label}, index) =>
+          index !== links.length - 1 ? (
+            <span key={label} className="flex items-center gap-1.5">
+              <Link href={url} className="hover:underline">
+                {label}
+              </Link>{' '}
+              <ArrowIcon size={16}/>
+            </span>
+          ) : (
+            <span className="cursor-default text-basic-blue font-medium" key={label}>
+              {label}
+            </span>
+          )
+        )}
+      </p>
+    )
+}
+  
---
+++ app/components/common/Header.js
@@ -0,0 +1,10 @@
+import Breadcrumb from "../Breadcrumb";
+
+export default function Header({ title, links = null }){
+    return(
+        <div className="flex flex-col gap-2">
+            {links && <Breadcrumb links={links}/>}
+            <div className="text-2xl font-bold text-dark-blue">{title}</div>
+        </div>
+    )
+}
