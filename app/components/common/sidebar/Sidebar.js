7cc02a715544fbc0fdada67c285710b9bacc6145
ervin-sungkono <ervinsung@gmail.com>
2024-02-05 21:34:04 +0700

    fix sidebar issue

--- app/components/common/sidebar/Sidebar.js
+++ app/components/common/sidebar/Sidebar.js
@@ -62,14 +62,14 @@ export default function Sidebar(){
     ]
 
     return(
-        <section className={`sidebar fixed lg:relative ${open ? "w-[200px] lg:w-[240px]" : "w-0 lg:w-20 "} bg-white flex-shrink-0 h-full flex flex-col shadow-md z-[10000] transition-all duration-300 ease-in-out`}>
+        <section className={`sidebar fixed lg:relative w-[200px] lg:translate-x-0 ${open ? "lg:w-[240px] translate-x-0" : "lg:w-20 -translate-x-full"} bg-white flex-shrink-0 h-full flex flex-col shadow-md z-[10000] transition-all duration-300 ease-in-out`}>
             <div className="w-full flex justify-between items-center px-4 pt-4 lg:pt-6 pb-4">  
                 <div className="text-sm md:text-base text-center font-semibold whitespace-nowrap overflow-hidden transition-[width] duration-300" style={{width: (open ? "100%" : "0px")}}>Project Name</div>
                 <button className="absolute top-2 lg:top-4 -right-6 lg:right-0 translate-x-1/2 text-white bg-basic-blue hover:bg-light-blue p-2.5 rounded-full transition-colors" onClick={toggleSidebar}>
                     {open ? <SidebarCollapseIcon size={20}/> : <SidebarExpandIcon size={20}/>}
                 </button>
             </div>
-            <div className={`w-full flex flex-col gap-4 px-4 pb-4 flex-grow transition-opacity duration-300 overflow-x-hidden divide-gray-300`}>
+            <div className={`w-full flex flex-col gap-4 pb-4 px-4 flex-grow transition-opacity duration-300 overflow-x-hidden divide-gray-300`}>
                 <SidebarMenu links={mainLinks} baseUrl={baseUrl} open={open}/>
                 <hr className="border-dark-blue/30"/>
                 <SidebarMenu links={settingLinks} baseUrl={baseUrl} open={open}/>
