bf372b3f4f82e0bc1ff0d480a76508968b617ee1
ervin-sungkono <ervinsung@gmail.com>
2024-03-12 15:37:19 +0700

    adjust popup form styling

--- app/components/common/alert/PopUpForm.js
+++ app/components/common/alert/PopUpForm.js
@@ -10,16 +10,16 @@ export default function PopUpForm({
     const getTitleSize = () => {
         switch(titleSize){
             case 'large':
-                return 'text-2xl md:text-3xl font-bold'
+                return 'text-xl md:text-3xl font-bold'
             case 'default':
-                return 'text-xl md:text-2xl font-semibold'
+                return 'text-lg md:text-2xl font-semibold'
         }
     }
 
     return (
         <PopUp>
             <div className={`flex flex-col gap-4 md:gap-6 px-6 py-4 md:px-8 md:py-6 bg-white text-dark-blue rounded-lg shadow-lg`}>
-                {title && message && <div className="flex flex-col gap-2">
+                {(title || message) && <div className="flex flex-col gap-2">
                     {title && <div className={`${getTitleSize()}`}>{title}</div>}
                     {message && <p className="text-xs md:text-sm">{message}</p>}
                 </div>}
