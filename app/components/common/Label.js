38be9c1d35ec003d2c2e08f6203c60af375d5933
ervin-sungkono <ervinsung@gmail.com>
2024-04-13 21:37:33 +0700

    adjut label component

--- app/components/common/Label.js
+++ app/components/common/Label.js
@@ -2,8 +2,8 @@ import { pickTextColorBasedOnBgColor } from "@/app/lib/color";
 
 export default function Label({ color = '#000000', text }){
     return(
-        <div className="rounded-full px-2 py-1 text-[10.8px] md:text-xs font-medium" style={{color: pickTextColorBasedOnBgColor(color), background: color}}>
-            {text}
+        <div className="flex h-[20px] items-center rounded-full px-2" style={{background: color}}>
+            <p className="text-xs leading-none font-medium" style={{color: pickTextColorBasedOnBgColor(color)}}>{text}</p>
         </div>
     )
 }
