8960a687da9fd5a8c470c38ea12541a30461a641
Christopher Vinantius <christopher.vinantius@binus.ac.id>
2024-04-18 16:53:10 +0700

    finished functionalities in history and notification pages

--- app/components/history/HistoryList.js
+++ app/components/history/HistoryList.js
@@ -1,52 +1,37 @@
 "use client"
 import HistoryItem from "./HistoryItem"
-import {
-    useReactTable,
-    getCoreRowModel,
-    getPaginationRowModel,
-    getSortedRowModel,
-} from '@tanstack/react-table'
 import HistoryPagination from "./HistoryPagination"
-import { useEffect } from "react"
 
-export default function HistoryList({historyData, pageSize=10}){
+export default function HistoryList({historyData, pageSize, pageIndex, setPageIndex, pageCount, dataCount}){
 
-    const { 
-        getState,
-        nextPage,
-        previousPage,
-        getPageCount,
-        getCanPreviousPage,
-        getCanNextPage, 
-        setPageSize,
-        setPageIndex
-    } = useReactTable({
-        getCoreRowModel: getCoreRowModel(),
-        getPaginationRowModel: getPaginationRowModel(),
-        getSortedRowModel: getSortedRowModel(),
-    })
+    let shownData
 
-    useEffect(() => {
-        if(setPageSize){
-            setPageSize(pageSize)
-        }
-    }, [setPageSize, pageSize])
-
-    return (
-        <div className="flex flex-col gap-2 md:gap-4">
-            {historyData.data.map((item, index) => (
-                <HistoryItem key={index} {...item}/>
-            ))}
-            {/* <HistoryPagination
-                {...getState().pagination}
-                hasPrevPage={getCanPreviousPage()}
-                hasNextPage={getCanNextPage()}
-                nextPage={nextPage}
-                previousPage={previousPage}
-                firstPage={() => setPageIndex(0)}
-                lastPage={() => setPageIndex(getPageCount() - 1)}
-                totalCount={data.length}
-            /> */}
-        </div>
-    )
+    if(historyData.length <= 0){
+        return (
+            <div className="text-center">
+                Tidak ada data riwayat yang tersedia.
+            </div>
+        )
+    }else{
+        shownData = historyData.data.slice(pageIndex * pageSize, (pageIndex + 1) * pageSize)
+        return (
+            <div className="flex flex-col gap-2 md:gap-4 mb-8">
+                {shownData.map((item, index) => (
+                    <HistoryItem key={index} {...item}/>
+                ))}
+                <HistoryPagination
+                    pageIndex={pageIndex}
+                    pageSize={pageSize}
+                    hasPrevPage={pageIndex > 0}
+                    hasNextPage={pageIndex < pageCount - 1}
+                    nextPage={() => setPageIndex(pageIndex + 1)}
+                    previousPage={() => setPageIndex(pageIndex - 1)}
+                    firstPage={() => setPageIndex(0)}
+                    lastPage={() => setPageIndex(pageCount - 1)}
+                    pageCount={pageCount}
+                    dataCount={dataCount}
+                />
+            </div>
+        )
+    }
 }
