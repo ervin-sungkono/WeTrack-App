ab50ddc4ad536b52a03a271cfd50a6d5826fb51d
ervin-sungkono <ervinsung@gmail.com>
2024-04-21 13:06:03 +0700

    fix issue with dynamic tailwind class naming

--- app/components/common/button/Button.js
+++ app/components/common/button/Button.js
@@ -1,3 +1,4 @@
+"use client"
 export default function Button({ 
     type = 'button', 
     outline = false,
@@ -12,48 +13,54 @@ export default function Button({
         switch(variant){
             case 'primary':
                 return ({
-                    bgColor: 'basic-blue',
-                    textColor: 'white',
+                    bgColor: 'bg-basic-blue',
+                    textColor: 'text-white',
+                    outlineTextColor: 'text-basic-blue',
                     borderClass: 'border-basic-blue',
                     hover: 'hover:bg-basic-blue/80',
                     hoverOutline: 'hover:bg-basic-blue hover:text-white'
                 })
             case 'secondary':
                 return ({
-                    bgColor: 'light-blue',
-                    textColor: 'dark-blue',
+                    bgColor: 'bg-light-blue',
+                    textColor: 'text-dark-blue',
+                    outlineTextColor: 'text-light-blue',
                     borderClass: 'border-light-blue',
                     hover: 'hover:bg-light-blue/80',
                     hoverOutline: 'hover:bg-light-blue hover:text-white'
                 })
             case 'success': 
                 return ({
-                    bgColor: 'success-green',
-                    textColor: 'white',
+                    bgColor: 'bg-success-green',
+                    textColor: 'text-white',
+                    outlineTextColor: 'text-success-green',
                     borderClass: 'border-success-green',
                     hover: 'hover:bg-success-green/80',
                     hoverOutline: 'hover:bg-success-green hover:text-white'
                 })
             case 'warning': 
                 return ({
-                    bgColor: 'warning-yellow',
-                    textColor: 'dark-blue',
+                    bgColor: 'bg-warning-yellow',
+                    textColor: 'text-dark-blue',
+                    outlineTextColor: 'text-warning-yellow',
                     borderClass: 'border-warning-yellow',
                     hover: 'hover:bg-warning-yellow/80',
                     hoverOutline: 'hover:bg-warning-yellow hover:text-dark-blue'
                 })
             case 'danger':
                 return({
-                    bgColor: 'danger-red',
-                    textColor: 'white',
+                    bgColor: 'bg-danger-red',
+                    textColor: 'text-white',
+                    outlineTextColor: 'text-danger-red',
                     borderClass: 'border-danger-red',
                     hover: 'hover:bg-danger-red/80',
                     hoverOutline: 'hover:bg-danger-red hover:text-white'
                 })
             case 'gray':
                 return({
-                    bgColor: 'gray-200',
-                    textColor: 'dark-blue',
+                    bgColor: 'bg-gray-200',
+                    textColor: 'text-dark-blue',
+                    outlineTextColor: 'text-gray-200',
                     borderClass: 'border-gray-400',
                     hover: 'hover:bg-gray-300',
                     hoverOutline: 'hover:bg-gray-300 hover:text-dark-blue'
@@ -62,6 +69,7 @@ export default function Button({
                 return ({
                     bgColor: '',
                     textColor: '',
+                    outlineTextColor: '',
                     borderClass: '',
                     hover: '',
                     hoverOutline: '',
@@ -72,25 +80,25 @@ export default function Button({
     const getSizeClass = () => {
         switch(size){
             case 'md':
-                return 'text-xs md:text-sm px-4 md:px-6 py-2 md:py-3'
+                return 'text-xs md:text-sm px-4 md:px-6 py-2 md:py-3 rounded-md'
             case 'sm':
-                return 'text-[10.8px] md:text-xs px-3 md:px-4 py-1.5 md:py-2'
+                return 'text-[10.8px] md:text-xs px-3 md:px-4 py-1.5 md:py-2 rounded'
         }
     }
 
     const getVariantClass = () => {
-        const { bgColor, textColor, borderClass, hover, hoverOutline } = getButtonColor()
+        const { bgColor, textColor, outlineTextColor, borderClass, hover, hoverOutline } = getButtonColor()
 
         if(outline){
-            return `border ${borderClass} text-${bgColor} ${hoverOutline}`
+            return `border ${borderClass} ${outlineTextColor} ${hoverOutline}`
         }
-        return `bg-${bgColor} text-${textColor} ${hover}`
+        return `${bgColor} ${textColor} ${hover}`
     }
 
     return(
         <button 
             type={type}
-            className={`${getVariantClass()} ${getSizeClass()} ${className} font-semibold rounded-md disabled:bg-gray-300 disabled:border-gray-300 disabled:text-gray-500 transition-colors duration-300 text-nowrap`}
+            className={`${getVariantClass()} ${getSizeClass()} ${className} font-semibold disabled:bg-gray-300 disabled:border-gray-300 disabled:text-gray-500 transition-colors duration-300 text-nowrap`}
             onClick={onClick}
             disabled={disabled}
         >
