aa7b4118d9b21a30dc4fa92a88748ab7b9748ae5
ervin-sungkono <ervinsung@gmail.com>
2024-05-15 21:40:51 +0700

    adjust styling for dashboard page

--- app/components/dashboard/DashboardTaskList.js
+++ app/components/dashboard/DashboardTaskList.js
@@ -2,11 +2,11 @@ import DashboardProjectItem from "./DashboardProjectItem"
 
 export default function DashboardTaskList({list, selectedProject, setSelectedProject}){
     return (
-        <div className="pb-6 bg-dark-gray rounded-lg">
-            <div className="p-4">
+        <div className="md:h-full md:overflow-y-auto bg-dark-gray rounded-lg pb-6 md:pb-0 custom-scrollbar">
+            <div className="p-4 sticky top-0 bg-dark-gray">
                 <div className="text-base md:text-lg font-bold">Daftar Tugas Saya</div>
             </div>
-            <div className="px-4 flex flex-col gap-2 max-h-[720px] overflow-x-hidden overflow-y-scroll">
+            <div className="px-4 flex flex-col gap-2 md:h-full md:overflow-y-auto">
                 {list.map((project, index) => (
                     <DashboardProjectItem key={index} project={project} selectedProject={selectedProject} setSelectedProject={setSelectedProject}/>
                 ))}
