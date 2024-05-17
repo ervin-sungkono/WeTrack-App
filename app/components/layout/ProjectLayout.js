fe4745f18343d665020cae5472df6160908117d6
ervin-sungkono <ervinsung@gmail.com>
2024-05-17 17:42:20 +0700

    add loading state and validate authorization in project page view

--- app/components/layout/ProjectLayout.js
+++ app/components/layout/ProjectLayout.js
@@ -2,22 +2,51 @@
 import Navbar from "../common/navbar/Navbar"
 import Sidebar from "../common/sidebar/Sidebar"
 import { useSessionStorage } from "usehooks-ts"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { getProjectByID } from "@/app/lib/fetch/project"
+import { useRouter } from "next/navigation"
+import { getContentAuthorization } from "@/app/firebase/util"
+import PopUpLoad from "../common/alert/PopUpLoad"
 
 export default function ProjectLayout({ children, hideSidebar, projectId }){
     const [project, setProject] = useSessionStorage('project')
+    const [loading, setLoading] = useState(false)
+    const router = useRouter()
+
     useEffect(() => {
-        if(projectId && (!project || project.id != projectId)){
-            getProjectByID(projectId)
-            .then(async(project) => {
-                if(project.data){
-                    setProject(project.data)
+        const fetchProjectDetail = async() => {
+            setLoading(true)
+            try{
+                const authorized = await getContentAuthorization({ projectId })
+                if(authorized){
+                    const project = await getProjectByID(projectId)
+                    if(project.data){
+                        setProject(project.data)
+                    }
+                    else {
+                        console.log("Gagal mendapatkan rincian proyek")
+                    }
+                    setLoading(false)
+                }else{
+                    router.replace('/projects')
                 }
-                else alert("Gagal memperoleh rincian proyek")
-            })
+            }catch(e){
+                console.log(e)
+                setLoading(false)
+            }
+        }
+        if(projectId && (!project || project.id != projectId)){
+            fetchProjectDetail()
         }
     }, [project, projectId])
+
+    if(loading){
+        return (
+            <div className="flex fixed top-0 left-0 bottom-0 right-0 bg-gray-100 pt-20">
+                <PopUpLoad/>
+            </div>
+        )
+    }
     
     return(
         <div className="flex fixed top-0 left-0 bottom-0 right-0 bg-gray-100 pt-20">
