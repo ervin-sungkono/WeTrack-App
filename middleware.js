5283dc518d8875faf2c3a3216cfd4b7b2fd8e387
ervin-sungkono <ervinsung@gmail.com>
2024-05-17 19:16:00 +0700

    adjust middleware for meta images

--- middleware.js
+++ middleware.js
@@ -1,4 +1,4 @@
 export { default } from "next-auth/middleware"
 
-export const config = { matcher: ["/((?!register|login|forgotPassword|api|_next/static|_next/image|images|favicon.ico|$).*)"] }
+export const config = { matcher: ["/((?!register|login|forgotPassword|api|_next/static|_next/image|images|favicon.ico|opengraph-image.png|twitter-image.png|$).*)"] }
 // Matcher untuk semua route kecuali halaman register, login, dan home  
