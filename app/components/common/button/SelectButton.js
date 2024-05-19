94c978c7f4728bf74d258610bd770d17a0e97a15
ervin-sungkono <ervinsung@gmail.com>
2024-05-19 15:56:57 +0700

    adjust defaultValue props in userSelectButton and SelectButton

--- app/components/common/button/SelectButton.js
+++ app/components/common/button/SelectButton.js
@@ -5,16 +5,20 @@ import { initDropdowns } from "flowbite"
 import { IoIosArrowDown as DropdownIcon } from 'react-icons/io'
 
 export default function SelectButton({ name, placeholder, defaultValue, options = [], onChange = null, buttonClass }){ 
-    const [selected, setSelected] = useState(defaultValue ?? options[0]?.label)
+    const [selected, setSelected] = useState(options[0])
     const buttonRef = useRef(null)
+
+    useEffect(() => {
+        if(defaultValue) setSelected(defaultValue)
+    }, [defaultValue])
 
     useEffect(() => {
         initDropdowns()
     })
 
-    const handleSelectedUpdate = (label, value) => { 
+    const handleSelectedUpdate = ({label, value}) => { 
         buttonRef.current.click()
-        setSelected(label)
+        setSelected({label, value})
         if(typeof onChange == "function") onChange(value)
     }
 
@@ -29,7 +33,7 @@ export default function SelectButton({ name, placeholder, defaultValue, options 
                 className={`flex items-center gap-1 font-semibold text-xs md:text-sm px-3 py-1.5 md:px-4 md:py-2 rounded-full focus:ring-0 focus:outline-none border transition-colors duration-300 ease-in-out ${buttonClass ?? 'text-dark-blue hover:text-basic-blue border-dark-blue/30'}`} 
                 type="button"
             >
-                {selected ?? placeholder} 
+                {selected?.label ?? placeholder} 
                 <DropdownIcon size={16}/>
             </button>
             <div id={name} className={`z-[100] hidden bg-white divide-y divide-gray-100 rounded-lg border border-dark-blue/30 min-w-40`}>
@@ -37,8 +41,8 @@ export default function SelectButton({ name, placeholder, defaultValue, options 
                     {options.map(({label, value}) => (
                         <li key={value}>
                             <button
-                                disabled={selected === label}
-                                onClick={() => handleSelectedUpdate(label, value)} className="block w-full text-start px-4 py-2 disabled:bg-gray-300 disabled:text-dark-blue hover:bg-gray-100 hover:text-basic-blue transition-colors duration-300 ease-in-out"
+                                disabled={selected?.label === label}
+                                onClick={() => handleSelectedUpdate({label, value})} className="block w-full text-start px-4 py-2 disabled:bg-gray-300 disabled:text-dark-blue hover:bg-gray-100 hover:text-basic-blue transition-colors duration-300 ease-in-out"
                             >
                                 {label}
                             </button>
