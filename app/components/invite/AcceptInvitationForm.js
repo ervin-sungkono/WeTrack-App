91035c1884625f2178ec161fd93a0ac1ad95e3b0
ervin-sungkono <ervinsung@gmail.com>
2024-05-21 11:10:04 +0700

    adjust invitation page

--- app/components/invite/AcceptInvitationForm.js
+++ app/components/invite/AcceptInvitationForm.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react"
 import { validateTeamMember } from "@/app/lib/fetch/team"
 import { IoClose } from "react-icons/io5"
 import { acceptInvite, rejectInvite } from "@/app/lib/fetch/invite"
+import PopUpLoad from "../common/alert/PopUpLoad"
 
 export default function AcceptInvitationForm({ teamId }){
     const searchParams = useSearchParams()
@@ -43,24 +44,25 @@ export default function AcceptInvitationForm({ teamId }){
     }
 
     if(!authorized) return(
-        <div className="w-full h-screen flex flex-col justify-center items-center px-6 pb-8">
+        <div className="w-full h-full flex flex-col justify-center items-center px-6 pb-8">
             <IoClose size={128} className="text-danger-red"/>
             <p className="text-sm md:text-base text-dark-blue/80 text-center">Link undangan tidak dapat digunakan.</p>
         </div>
     )
     if(rejected) return(
-        <div className="w-full h-screen flex flex-col justify-center items-center px-6 pb-8">
+        <div className="w-full h-full flex flex-col justify-center items-center px-6 pb-8">
             <p className="text-sm md:text-base text-dark-blue/80 text-center">Undangan telah berhasil ditolak</p>
         </div>
     )
     if(accepted) return(
-        <div className="w-full h-screen flex flex-col justify-center items-center px-6 pb-8">
+        <div className="w-full h-full flex flex-col justify-center items-center px-6 pb-8">
             <p className="text-sm md:text-base text-dark-blue/80 text-center">Undangan telah berhasil diterima</p>
         </div>
     )
 
     return(
-        <div className="w-full h-screen flex justify-center items-center px-6 pb-8">
+        <div className="w-full h-full flex justify-center items-center px-6 pb-8">
+            {loading && <PopUpLoad/>}
             <div className="w-full max-w-2xl flex flex-col gap-4 bg-white px-6 md:px-8 py-4 md:py-6 rounded-md shadow-md m-auto">
                 <div className="flex flex-col gap-1.5">
                     <div className="text-lg md:text-2xl font-bold text-dark-blue">Terima Undangan</div>
