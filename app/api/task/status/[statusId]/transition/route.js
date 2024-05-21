6eab18b1ac386c126cdad286a07207005b935b08
ervin-sungkono <ervinsung@gmail.com>
2024-05-21 09:29:13 +0700

    fix status api transition

--- app/api/task/status/[statusId]/transition/route.js
+++ app/api/task/status/[statusId]/transition/route.js
@@ -40,7 +40,7 @@ export async function POST(request, response) {
             }, { status: 404 });
         }
 
-        const projectRole = getProjectRole({projectId, userId})
+        const projectRole = await getProjectRole({projectId, userId})
         if(projectRole !== 'Owner'){
             return NextResponse.json({
                 message: "Unauthorized",
