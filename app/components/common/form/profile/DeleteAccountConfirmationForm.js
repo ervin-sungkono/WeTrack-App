3bed4bd646b533d68544b2a8a9d4571f1343e778
Christopher Vinantius <christopher.vinantius@binus.ac.id>
2024-05-29 23:31:51 +0700

    adjusted danger button placements

--- app/components/common/form/profile/DeleteAccountConfirmationForm.js
+++ app/components/common/form/profile/DeleteAccountConfirmationForm.js
@@ -11,8 +11,8 @@ export default function DeleteAccountConfirmationForm({nextFormStep, onClose}){
             wrapContent
         >
             <div className="flex flex-col xs:flex-row justify-end gap-2 md:gap-4">
-                <Button variant="secondary" onClick={onClose}>Tidak</Button>
                 <Button variant="danger" onClick={nextFormStep}>Ya</Button>
+                <Button variant="secondary" onClick={onClose}>Tidak</Button>
             </div>
         </PopUpForm>
     )
