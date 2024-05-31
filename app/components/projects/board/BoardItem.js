c3c3593fa44a181708f271df4f28fe5b0914951d
ervin-sungkono <ervinsung@gmail.com>
2024-05-31 23:12:10 +0700

    fix update task name error not validating

--- app/components/projects/board/BoardItem.js
+++ app/components/projects/board/BoardItem.js
@@ -58,12 +58,11 @@ export default function BoardItem({ item, index }){
       }
     ]
 
-    const handleUpdateTaskName = async(e, taskName) => {
-      e.stopPropagation()
+    const handleUpdateTaskName = async(values) => {
       setLoading(true)
 
       try{
-        if(taskName !== item.taskName) await updateTask({ taskId: item.id, taskName })
+        if(values.taskName !== item.taskName) await updateTask({ taskId: item.id, taskName: values.taskName })
       }catch(e){
         console.log(e)
       }finally{
@@ -88,83 +87,83 @@ export default function BoardItem({ item, index }){
 
     const {label: priorityLabel, color: priorityColor} = getPriority(item.priority)
     return (
-      <Draggable draggableId={item.id} index={index} isDragDisabled={!validateUserRole({ userRole: role, minimumRole: 'Member'})}>
-        {(provided, snapshot) => (
-          <div
-              ref={provided.innerRef}
-              {...provided.draggableProps}
-              {...provided.dragHandleProps}
-              style={getItemStyle(
-                  snapshot.isDragging,
-                  provided.draggableProps.style
-              )}
-              onClick={() => viewTask(item.id)}
-          >
-            {loading && <PopUpLoad/>}
-            {updateConfirmation &&
-              <UpdateTaskNameForm 
-                taskName={item.taskName} 
-                onSubmit={handleUpdateTaskName} 
-                onClose={(e) => {
-                  e.stopPropagation()
-                  setUpdateConfirmation(false)
-                }}
-              />
-            }
-            {deleteConfirmation &&
-              (<PopUpForm
-                title={"Hapus Tugas"}
-                message={'Apakah Anda yakin ingin menghapus tugas ini?'}
-                wrapContent
-              >
-                <>
-                  <div className="mt-4 flex flex-col xs:flex-row justify-end gap-2 md:gap-4">
-                    <Button variant="danger" onClick={handleDeleteTask}>Hapus</Button>
-                    <Button variant="secondary" onClick={(e) => {
-                        e.stopPropagation()
-                        setDeleteConfirmation(false)
-                      }}
-                    >Batal</Button>
+        <Draggable draggableId={item.id} index={index} isDragDisabled={!validateUserRole({ userRole: role, minimumRole: 'Member'})}>
+          {(provided, snapshot) => (
+            <div
+                ref={provided.innerRef}
+                {...provided.draggableProps}
+                {...provided.dragHandleProps}
+                style={getItemStyle(
+                    snapshot.isDragging,
+                    provided.draggableProps.style
+                )}
+                onClick={(updateConfirmation || deleteConfirmation) ? null : () => viewTask(item.id)}
+            >
+              {loading && <PopUpLoad/>}
+              {updateConfirmation &&
+                <UpdateTaskNameForm 
+                  taskName={item.taskName} 
+                  onSubmit={handleUpdateTaskName} 
+                  onClose={(e) => {
+                    e.stopPropagation()
+                    setUpdateConfirmation(false)
+                  }}
+                />
+              }
+              {deleteConfirmation &&
+                (<PopUpForm
+                  title={"Hapus Tugas"}
+                  message={'Apakah Anda yakin ingin menghapus tugas ini?'}
+                  wrapContent
+                >
+                  <>
+                    <div className="mt-4 flex flex-col xs:flex-row justify-end gap-2 md:gap-4">
+                      <Button variant="danger" onClick={handleDeleteTask}>Hapus</Button>
+                      <Button variant="secondary" onClick={(e) => {
+                          e.stopPropagation()
+                          setDeleteConfirmation(false)
+                        }}
+                      >Batal</Button>
+                    </div>
+                  </>
+                </PopUpForm>)
+              }
+              <div className="flex flex-col">
+                <div className="flex items-center gap-2">
+                  <p className="flex-grow text-xs md:text-sm font-semibold py-1.5">{item.taskName}</p>
+                  {validateUserRole({ userRole: role, minimumRole: 'Member' }) && 
+                  <DotButton 
+                    name={`task-${item.id}`}
+                    actions={taskActions}
+                  />}
+                </div>
+                {item.labels && (
+                  <div className="flex flex-wrap gap-1">
+                    {item.labels.map(({id, content, backgroundColor}) => (
+                      <Label key={id} text={content} color={backgroundColor}/>
+                    ))}
                   </div>
-                </>
-              </PopUpForm>)
-            }
-            <div className="flex flex-col">
-              <div className="flex items-center gap-2">
-                <p className="flex-grow text-xs md:text-sm font-semibold py-1.5">{item.taskName}</p>
-                {validateUserRole({ userRole: role, minimumRole: 'Member' }) && 
-                <DotButton 
-                  name={`task-${item.id}`}
-                  actions={taskActions}
-                />}
+                )}
               </div>
-              {item.labels && (
-                <div className="flex flex-wrap gap-1">
-                  {item.labels.map(({id, content, backgroundColor}) => (
-                    <Label key={id} text={content} color={backgroundColor}/>
-                  ))}
-                </div>
-              )}
+              <div className="flex items-center gap-2">
+                  <div className="flex flex-grow items-center gap-1 text-dark-blue">
+                    <CheckIcon size={16}/>
+                    <p className="text-[10px] md:text-xs">{project && <span>{project.key}-{item.displayId}</span>}</p>
+                  </div>
+                  {item.priority !== 0 && 
+                  <CustomTooltip id={`task-${item.id}-priority-tooltip`} content={`Prioritas: ${priorityLabel}`}>
+                    <div className="w-4 h-4 rounded-full" style={{backgroundColor: priorityColor}}></div>
+                  </CustomTooltip>}
+                  <CustomTooltip id={`task-${item.id}-tooltip`} content={item.assignedTo?.fullName ?? "Belum Ditugaskan"}>
+                    <UserIcon 
+                      size="sm"
+                      src={item.assignedTo ? item.assignedTo.profileImage?.attachmentStoragePath : '/images/user-placeholder.png'}
+                      fullName={item.assignedTo?.fullName}
+                    />
+                  </CustomTooltip>
+              </div>
             </div>
-            <div className="flex items-center gap-2">
-                <div className="flex flex-grow items-center gap-1 text-dark-blue">
-                  <CheckIcon size={16}/>
-                  <p className="text-[10px] md:text-xs">{project && <span>{project.key}-{item.displayId}</span>}</p>
-                </div>
-                {item.priority !== 0 && 
-                <CustomTooltip id={`task-${item.id}-priority-tooltip`} content={`Prioritas: ${priorityLabel}`}>
-                  <div className="w-4 h-4 rounded-full" style={{backgroundColor: priorityColor}}></div>
-                </CustomTooltip>}
-                <CustomTooltip id={`task-${item.id}-tooltip`} content={item.assignedTo?.fullName ?? "Belum Ditugaskan"}>
-                  <UserIcon 
-                    size="sm"
-                    src={item.assignedTo ? item.assignedTo.profileImage?.attachmentStoragePath : '/images/user-placeholder.png'}
-                    fullName={item.assignedTo?.fullName}
-                  />
-                </CustomTooltip>
-            </div>
-          </div>
-        )}
-      </Draggable>
+          )}
+        </Draggable> 
     );
 }
